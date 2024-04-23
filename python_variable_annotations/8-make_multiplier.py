6f1157bc3559926ed3030206d042857ca1660c88
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2024-04-23 15:34:52 +0100

    multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Multiplier """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Creates a function that multiplies a number by a given multiplier.
+    """
+    return multiplier_function(multiplier)
+
+
+def multiplier_function(n: float):
+    """
+    Multiplies a number by the given multiplier.
+    """
+    return lambda x: x * n
