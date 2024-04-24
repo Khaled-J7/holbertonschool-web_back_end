610140b18ab6fa7eef2b24b42fd2093395bdae01
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2024-04-24 21:14:03 +0100

    add random module

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,17 +1,11 @@
-
 #!/usr/bin/env python3
-
-'''
-a function (do not create an async function,
-use the regular function syntax to do this)
-task_wait_random that takes an integer max_delay and returns a asyncio.Task
-'''
-
+"""takes an integer max_delay and returns a asyncio.Task"""
 import asyncio
+import random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    '''Function which return an asyncio task object'''
+    """awaits a random float and returns it"""
     return asyncio.create_task(wait_random(max_delay))
