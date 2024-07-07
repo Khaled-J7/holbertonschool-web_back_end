9e9b514e1a104eeb0b2ab5c5dd2fa285568a2872
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2024-07-07 23:57:34 +0100

    Fix VROOM

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,12 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
 export default class Car {
-  constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    cloneCar() {
+      const car = Object.getPrototypeOf(this);
+      return new car.constructor();
+    }
   }
-
-  cloneCar() {
-    return new this.constructor(this[_brand], this[_motor], this[_color]);
-  }
-}
