f5c47cc8bd376ed504210dbd6a72cebff377c5c9
Khaled Jallouli <6711@holbertonstudents.com>
2025-03-07 04:22:40 +0100

    pycodestyle fix

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
-Basic Flask app 
+Basic Flask app
 """
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') # type: ignore
+
+@app.route("/")  # type: ignore
 def index():
-    render_template('index.html')
-    
+    render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
