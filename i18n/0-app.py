e5a26e5a17794fb94ae234cbece821aca9854aa2
Khaled Jallouli <6711@holbertonstudents.com>
2025-03-07 04:33:25 +0100

    fix

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -4,12 +4,13 @@ Basic Flask app that renders a template with a welcome message.
 """
 
 from flask import Flask, render_template
+from typing import Any
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index() -> str:
     """Render the index template."""
     return render_template("0-index.html")
 
